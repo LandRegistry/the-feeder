d92bb930a2dcc0cc163b5bf1a1ae783ee0ed77cf
Skablam <stainlesssteelmatt@gmail.com>
2014-07-17 13:55:00 +0100

    modified worker to assume the data in the redis queue is in line with the agreed test data structure and modifed the public title structure

--- worker.py
+++ worker.py
@@ -14,11 +14,22 @@ def process_queue_items():
         try:
             msg = redis.blpop(redis_queue)
             payload = msg[1].replace('\'', '\"')
+
             json_data = json.loads(payload)
-            title_number = json_data['title_number']
+            title_number = json_data['title_id']
+
+            #build public title data structure
+            public_title = {"title_number": title_number,
+            "house_number" : json_data['property']['address']['house_number'],
+            "road" : json_data['property']['address']['road'],
+            "town" : json_data['property']['address']['town'],
+            "postcode" : json_data['property']['address']['postcode'],
+            "price_paid": json_data['payment']['price_paid']
+            }
+
             title_url = "%s/%s" % (titles_api_url, title_number)
             header = {"Content-Type": "application/json"}
-            r = requests.post(title_url,  data=payload, headers=header)
+            r = requests.post(title_url,  data=json.dumps(public_title), headers=header)
             print r.status_code
         except Exception, e:
             print e
